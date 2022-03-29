42cd280d33b3fbb4e9e6bf8678a07354bbf254cd
beibei1027bxb <a13133563340@163.com>
2022-03-29 13:10:15 +0800

    1231515616sssadasdasdasdasd

--- Rename_test.java
+++ Rename_test.java
@@ -1,6 +1,6 @@
 public class Rename_test {
     //inline
-    public static void vvv(int w,int c){
+    public static void ssssss(int w,int c,int m){
         int a=10,b=20;
         System.out.println(a+b);
     }
