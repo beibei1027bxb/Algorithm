d5b9f58eace46c20d8dfbbf9b5598b15a1600219
beibei1027bxb <a13133563340@163.com>
2022-03-31 14:59:58 +0800

    3

--- Rename_test.java
+++ Rename_test.java
@@ -2,13 +2,19 @@ public class Rename_test {
     //inline
     private static int ssssss(int w,int c,int m){
         int a=10,b=20,c=30,d=40;
-        System.out.println(a+b);
+        a(a,b,c,d);
 
         return a;
+    }
+    public static void a(int a,int b,int c,int d){
+        System.out.println(a+b);
     }
     public static void b(int a,int b,int c,int d){
         System.out.println(a+c);
         System.out.println(b+d);
+    }
+    public static void c(int a,int b,int c,int d){
+        a(a,b,c,d);
     }
 
 
