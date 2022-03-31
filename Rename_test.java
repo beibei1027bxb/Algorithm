65eb3370663def03b996eabd71c8a68e4831a7ae
beibei1027bxb <a13133563340@163.com>
2022-03-31 14:54:21 +0800

    3

--- Rename_test.java
+++ Rename_test.java
@@ -2,7 +2,7 @@ public class Rename_test {
     //inline
     private static int ssssss(int w,int c,int m){
         int a=10,b=20,c=30,d=40;
-        System.out.println(a+b);
+        a(a,b,c,d);
 
         return a;
     }
