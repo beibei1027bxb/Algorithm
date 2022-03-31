2dda4724fec05fdb8f4337f083e1d9c43d86214e
beibei1027bxb <a13133563340@163.com>
2022-03-31 14:32:23 +0800

    1231515616sssadasdasdasdasd

--- Rename_test.java
+++ Rename_test.java
@@ -3,21 +3,30 @@ public class Rename_test {
     private static int ssssss(int w,int c,int m){
         int a=10,b=20,c=30,d=40;
 
-        System.out.println(a+b);
-        System.out.println(c+d);
-        System.out.println(a+b);
-        System.out.println(c+d);
-        System.out.println(a+b);
-        System.out.println(c+d);
-        System.out.println(a+b);
-        System.out.println(c+d);
-        System.out.println(a+b);
-        System.out.println(c+d);
-        System.out.println(a+b);
-        System.out.println(c+d);
+        a(a,b,c,d);
+        aa(a,b,c,d);
+
         return a;
     }
+    public static void a(int a,int b,int c,int d){
+        System.out.println(a+b);
+        System.out.println(c+d);
+        System.out.println(a+b);
+        System.out.println(c+d);
+        System.out.println(a+b);
+        System.out.println(c+d);
+        System.out.println(a+b);
+        System.out.println(c+d);
+        return;
 
+    }
+    public static void aa(int a,int b,int c,int d){
+        System.out.println(a+b);
+        System.out.println(c+d);
+        System.out.println(a+b);
+        System.out.println(c+d);
+        return;
+    }
 
 }
 
