970a8e94827f45545f3287b94c5e16081a25370e
beibei1027bxb <a13133563340@163.com>
2022-04-01 11:28:06 +0800

    4

---
+++ test/refactor.java
@@ -0,0 +1,9 @@
+public class refactor {
+    Thread thread=new Thread(new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("hello world");
+        }
+    });
+
+}
