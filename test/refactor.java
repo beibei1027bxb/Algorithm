10900587631fc00f24ecac6994007056c9a1f8bd
beibei1027bxb <a13133563340@163.com>
2022-04-01 11:36:55 +0800

    4

--- test/refactor.java
+++ test/refactor.java
@@ -3,19 +3,18 @@ import com.sun.istack.internal.NotNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class refactor {
     public static void main(String[] args) {
         List<String> fruits = Arrays.asList("apple", "orange", "banana");
 
-        // method one 使用循环
-        List<String> resultsOfLoop = getFilterOutput(fruits, "orange");
-        for (String result : resultsOfLoop) {
-            System.out.println(result);
-        }
+        List<String> resultsOfPipeline =
+                fruits.stream().filter(fruit -> !"orange".equals(fruit)).collect(Collectors.toList());
+        resultsOfPipeline.forEach(System.out::println);
     }
 
-    private static List<String> getFilterOutput(List<String> fruits, @NotNull  String filter) {
+    private static List<String> getFilterOutput(List<String> fruits, @NotNull String filter) {
         List<String> result = new ArrayList<>();
         for (String fruit : fruits) {
             if (!filter.equals(fruit)) {
