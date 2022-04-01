e939832becc1e8f25cb49653b1d1005d2dfea959
beibei1027bxb <a13133563340@163.com>
2022-04-01 11:36:26 +0800

    4

--- test/refactor.java
+++ test/refactor.java
@@ -1,4 +1,28 @@
+import com.sun.istack.internal.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class refactor {
-    Thread thread=new Thread(()-> System.out.println("hello"));
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("apple", "orange", "banana");
+
+        // method one 使用循环
+        List<String> resultsOfLoop = getFilterOutput(fruits, "orange");
+        for (String result : resultsOfLoop) {
+            System.out.println(result);
+        }
+    }
+
+    private static List<String> getFilterOutput(List<String> fruits, @NotNull  String filter) {
+        List<String> result = new ArrayList<>();
+        for (String fruit : fruits) {
+            if (!filter.equals(fruit)) {
+                result.add(fruit);
+            }
+        }
+        return result;
+    }
 
 }
