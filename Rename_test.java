a2e4821fee785336fc38dbeddf858e568ae9d5de
beibei1027bxb <90845043+beibei1027bxb@users.noreply.github.com>
2022-05-09 14:14:28 +0800

    Update Rename_test.java
    sadasdasdas

--- Rename_test.java
+++ Rename_test.java
@@ -1,15 +1,13 @@
 
-import file;
 public class Rename_test {
     //inline
-    private static int ssssss(int w,int c,int m){
-        int a=10,b=20,c=30,d=40,sum=0;
-        file.bb(int a,int b,int c,int d);
-        for (int i = 0; i < 10; i++) {
-            sum+=i;
-        }
-
-        return a;
+    private static int ssssss(int w,int m){
+        int a=10,b=20,c=30,d=40;
+        System.out.println(a+b);
+        System.out.println(c+d);
+        System.out.println(a+c);
+        System.out.println(b+d);
+        return 0;
     }
 
 
@@ -17,4 +15,3 @@ public class Rename_test {
 
 
 }
-
