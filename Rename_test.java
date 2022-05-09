4982304840d28410911177404d568371e24cce97
beibei1027bxb <90845043+beibei1027bxb@users.noreply.github.com>
2022-05-09 14:16:38 +0800

    Update Rename_test.java
    sadasd

--- Rename_test.java
+++ Rename_test.java
@@ -3,12 +3,23 @@ public class Rename_test {
     //inline
     private static int ssssss(int w,int m){
         int a=10,b=20,c=30,d=40;
+        a(a,b,c,d);
+        b(a,b,c,d);
+        int sum=0;
+        for (int i = 0; i < 100; i++) {
+            sum+=i;
+        }
+        return sum;
+    }
+    private static void a(int a,int b,int c,int d){
         System.out.println(a+b);
         System.out.println(c+d);
+    }
+    private static void b(int a,int b,int c,int d){
         System.out.println(a+c);
         System.out.println(b+d);
-        return 0;
     }
+
 
 
 
