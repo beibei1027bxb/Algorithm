e533664b624d8b68456a58e2c83f5e8959cfc13e
beibei1027bxb <90845043+beibei1027bxb@users.noreply.github.com>
2022-05-09 14:30:16 +0800

    Update Rename_test.java
    sadas

--- Rename_test.java
+++ Rename_test.java
@@ -1,37 +1,17 @@
-
 public class Rename_test {
     //inline
-    private static int ssssss(int w,int m){
+    private static int ssssss(int w,int c,int m){
         int a=10,b=20,c=30,d=40;
         a(a,b,c,d);
-        b(a,b,c,d);
-        int sum=0;
-        for (int i = 0; i < 100; i++) {
-            sum+=i;
-        }
-        return sum;
+
+        return a;
     }
-    private static void a(int a,int b,int c,int d){
+    public static void a(int a,int b,int c,int d){
         System.out.println(a+b);
-        System.out.println(c+d);
+        System.out.println(a+c);
     }
-    private static void b(int a,int b,int c,int d){
+    public static void b(int a,int b,int c,int d){
         System.out.println(a+c);
         System.out.println(b+d);
     }
-    void renderBanner() {
-  final boolean isMacOs = platform.toUpperCase().indexOf("MAC") > -1;
-  final boolean isIE = browser.toUpperCase().indexOf("IE") > -1;
-  final boolean wasResized = resize > 0;
-
-  if (isMacOs && isIE && wasInitialized() && wasResized) {
-    // do something
-  }
-}
-
-
-
-
-
-
 }
