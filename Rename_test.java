62aea04410717b7958f36c3ef228711d0c974f4b
beibei1027bxb <90845043+beibei1027bxb@users.noreply.github.com>
2022-05-09 14:21:13 +0800

    Update Rename_test.java
    asdasd

--- Rename_test.java
+++ Rename_test.java
@@ -19,6 +19,14 @@ public class Rename_test {
         System.out.println(a+c);
         System.out.println(b+d);
     }
+    void renderBanner() {
+  if ((platform.toUpperCase().indexOf("MAC") > -1) &&
+       (browser.toUpperCase().indexOf("IE") > -1) &&
+        wasInitialized() && resize > 0 )
+  {
+    // do something
+  }
+}
 
 
 
