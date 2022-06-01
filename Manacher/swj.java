4ef569f018f43784c5bfb470a244be614a455933
beibei1027bxb <a13133563340@163.com>
2022-06-01 14:34:03 +0800

    123

--- Manacher/swj.java
+++ Manacher/swj.java
@@ -8,7 +8,8 @@ import java.io.FileReader;
 public class swj {
     public static void b  (@NamedArg("123441241") int a, int b, int c, int d) throws FileNotFoundException {
 
-        String fullname = "Robert King";
+        String first = "Robert";
+        String last="King";
         int m=10;
         int n=20;
         Thread thread=new Thread(()-> System.out.println("hello"));
