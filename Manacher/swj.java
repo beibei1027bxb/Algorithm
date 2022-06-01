38f9555f29f0121b3ce0f3bca91f0b925bb62ea9
beibei1027bxb <a13133563340@163.com>
2022-06-01 14:17:41 +0800

    123

--- Manacher/swj.java
+++ Manacher/swj.java
@@ -1,11 +1,12 @@
 import com.sun.istack.internal.NotNull;
+import javafx.beans.NamedArg;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class swj {
-    public static void b  (@NotNull int a,int b,int c,int d) throws FileNotFoundException {
+    public static void b  (@NamedArg("123") int a, int b, int c, int d) throws FileNotFoundException {
 
         int m=20;
         Thread thread=new Thread(()-> System.out.println("hello"));
