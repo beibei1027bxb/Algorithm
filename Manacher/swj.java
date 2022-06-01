89543c1cb75df55923add4833082883985cb8017
beibei1027bxb <a13133563340@163.com>
2022-06-01 14:02:53 +0800

    123

--- Manacher/swj.java
+++ Manacher/swj.java
@@ -3,10 +3,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class swj {
-    public static void b  (int a,int b,int c,int d)  {
-        System.out.println(a+c);
-        System.out.println(b+d);
+    public static void b  (int a,int b,int c,int d) throws FileNotFoundException {
 
-
+            Thread thread=new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("hello world");
+                }
+            });
     }
 }
