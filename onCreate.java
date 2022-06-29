e8a18e1a9a7e001866c97ac3d98aecd295f6cd1b
beibei1027bxb <90845043+beibei1027bxb@users.noreply.github.com>
2022-06-29 16:17:23 +0800

    original

---
+++ onCreate.java
@@ -0,0 +1,29 @@
+public class onCreate {
+    /**
+     * create the NavigationUI and handle the button.
+     *
+     * @param savedInstanceState
+     */
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        handleFabButton.setup(mAppBarConfiguration,navController);
+    }
+   
+}
